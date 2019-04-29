cf65fb5aee3efd60226b6d172ca53ad4fbc3ae6e
maryamNazarieh <maryam.nazarieh@gmail.com>
2019-04-29 12:19:55 +0200

    Update GRidentification.R

--- GRidentification.R
+++ GRidentification.R
@@ -2,7 +2,7 @@
 ESC_set = c()
 for(i in 1:dim(exp.data)[1]){
   S = 0
-  if(exp.data[i,3] < exp.data[i,2] & exp.data[i,4] < exp.data[i,2] & exp.data[i,5] < exp.data[i,2] & exp.data[i,6] < exp.data[i,2] & exp.data[i,7] < exp.data[i,2]){
+  if(exp.data[i,2] == max(exp.data[i,-1])){
     if(exp.data[i,4] < exp.data[i,3])
       S = S + 1
     if(exp.data[i,5] < exp.data[i,4])
@@ -21,9 +21,7 @@ for(i in 1:dim(exp.data)[1]){
 MES_set = c()
 for(i in 1:dim(exp.data)[1]){
   S = 0
-  if(exp.data[i,3] > exp.data[i,2]){ 
-    if(exp.data[i,4] < exp.data[i,3])
-      S = S + 1
+  if(exp.data[i,3] == max(exp.data[i,-1])){
     if(exp.data[i,5] < exp.data[i,4])
       S = S + 1
     if(exp.data[i,6] < exp.data[i,5])
@@ -31,65 +29,59 @@ for(i in 1:dim(exp.data)[1]){
     if(exp.data[i,7] < exp.data[i,6])
       S = S + 1
   }
-  if(S > 2)
-    MES_set = c(MES_set,as.character(exp.data[i,1]))
+    if(S > 1)
+      MES_set = c(MES_set,as.character(exp.data[i,1]))
 }
 
 HB_set = c()
 for(i in 1:dim(exp.data)[1]){
   S = 0
-  if(exp.data[i,4] > exp.data[i,3] & exp.data[i,4] > exp.data[i,2]){ 
+  if(exp.data[i,4] == max(exp.data[i,-1])){
     if(exp.data[i,2] < exp.data[i,3])
-      S = S + 1
-    if(exp.data[i,5] < exp.data[i,4])
       S = S + 1
     if(exp.data[i,6] < exp.data[i,5])
       S = S + 1
     if(exp.data[i,7] < exp.data[i,6])
       S = S + 1
   }
-  if(S > 2)
+  if(S > 1)
     HB_set = c(HB_set,as.character(exp.data[i,1]))
 }
 
 HE_set = c()
 for(i in 1:dim(exp.data)[1]){
   S = 0
-  if(exp.data[i,5] > exp.data[i,2] & exp.data[i,5] > exp.data[i,3]  & exp.data[i,5] > exp.data[i,4]){ 
+  if(exp.data[i,5] == max(exp.data[i,-1])){
     if(exp.data[i,2] < exp.data[i,3])
       S = S + 1
     if(exp.data[i,3] < exp.data[i,4])
       S = S + 1
-    if(exp.data[i,6] < exp.data[i,5])
-      S = S + 1
     if(exp.data[i,7] < exp.data[i,6])
       S = S + 1
   }
-  if(S > 2)
+  if(S > 1)
     HE_set = c(HE_set,as.character(exp.data[i,1]))
 }
 
 HP_set = c()
 for(i in 1:dim(exp.data)[1]){
   S = 0
-  if(exp.data[i,6] > exp.data[i,2] & exp.data[i,6] > exp.data[i,3]  & exp.data[i,6] > exp.data[i,4] & exp.data[i,6] > exp.data[i,5]){ 
+  if(exp.data[i,6] == max(exp.data[i,-1])){
     if(exp.data[i,2] < exp.data[i,3])
       S = S + 1
     if(exp.data[i,3] < exp.data[i,4])
       S = S + 1
     if(exp.data[i,4] < exp.data[i,5])
       S = S + 1
-    if(exp.data[i,7] < exp.data[i,6])
-      S = S + 1
   }
-  if(S > 2)
+  if(S > 1)
     HP_set = c(HP_set,as.character(exp.data[i,1]))
 }
 
 MAC_set = c()
 for(i in 1:dim(exp.data)[1]){
   S = 0
-  if(exp.data[i,7] > exp.data[i,2] & exp.data[i,7] > exp.data[i,3]  & exp.data[i,7] > exp.data[i,4] & exp.data[i,7] > exp.data[i,5] & exp.data[i,7] > exp.data[i,6]){ 
+  if(exp.data[i,7] == max(exp.data[i,-1])){
     if(exp.data[i,2] < exp.data[i,3])
       S = S + 1
     if(exp.data[i,3] < exp.data[i,4])
@@ -102,9 +94,3 @@ for(i in 1:dim(exp.data)[1]){
   if(S > 2)
     MAC_set = c(MAC_set,as.character(exp.data[i,1]))
 }
-length(ESC_set)
-length(MES_set)
-length(HB_set)
-length(HE_set)
-length(HP_set)
-length(MAC_set)
